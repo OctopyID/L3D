c889be28fde18112a47796da79122d615efa1ace
Supian M <supianidz@gmail.com>
2023-12-02 01:37:32 +0800

    WIP

--- src/Console/Commands/DomainMakeCommand.php
+++ src/Console/Commands/DomainMakeCommand.php
@@ -34,6 +34,39 @@ class DomainMakeCommand extends GeneratorCommand
             return $this->components->error($this->type . ' already exists.');
         }
 
+        $this
+            ->createDirectories($location)
+            ->createFiles($location);
+
+        $this->components->info(sprintf(
+            '%s [%s] created successfully.', $this->type, $this->getNameInput()
+        ));
+    }
+
+    /**
+     * Get the stub file for the generator.
+     */
+    protected function getStub()
+    {
+        //
+    }
+
+    /**
+     * @return array[]
+     */
+    protected function getOptions() : array
+    {
+        return [
+            ['force', null, InputOption::VALUE_NONE, 'Create the class even if the controller already exists'],
+        ];
+    }
+
+    /**
+     * @param  string $location
+     * @return $this
+     */
+    private function createDirectories(string $location) : self
+    {
         $paths = [
             'Models',
             'Providers',
@@ -45,35 +78,29 @@ class DomainMakeCommand extends GeneratorCommand
             $this->files->makeDirectory(sprintf('%s/%s', $location, $path), 0755, true, true);
         }
 
+        return $this;
+    }
+
+    /**
+     * @param  string $location
+     * @return void
+     */
+    private function createFiles(string $location) : void
+    {
         $files = [
             'web.php',
             'api.php',
         ];
 
+        $content = <<<EOT
+            <?php
+            
+            use Illuminate\Support\Facades\Route;
+            
+            EOT;
+
         foreach ($files as $file) {
-            $this->files->put(sprintf('%s/%s', $location, $file), $this->getStub());
+            $this->files->put(sprintf('%s/%s', $location, $file), $content);
         }
-
-        $this->components->info(sprintf(
-            '%s [%s] created successfully.', $this->type, $this->getNameInput()
-        ));
-    }
-
-    /**
-     * @return void
-     */
-    protected function getStub()
-    {
-        // TODO: Implement getStub() method.
-    }
-
-    /**
-     * @return array[]
-     */
-    protected function getOptions() : array
-    {
-        return [
-            ['force', null, InputOption::VALUE_NONE, 'Create the class even if the controller already exists'],
-        ];
     }
 }
