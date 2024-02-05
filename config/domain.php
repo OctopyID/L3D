257462c4d3ac3c50b93418c47322cd75652d780f
Supian M <supianidz@gmail.com>
2024-02-05 13:39:54 +0800

    WIP

---
+++ config/domain.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\App;
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Domain Location
+    |--------------------------------------------------------------------------
+    |
+    | The location of the domain.
+    |
+    */
+
+    'path' => app_path('Domains')
+];
