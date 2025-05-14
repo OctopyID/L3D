e4b244245d18ea7a101e75a352382fe16f1e194c
Supian M <supianidz@gmail.com>
2025-05-14 17:46:45 +0800

    test: add unit tests for Domain

---
+++ tests/Unit/DomainTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Octopy\Tests\Unit;
+
+use Octopy\L3D\Domain;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class DomainTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    #[Test]
+    public function canCreateDomainInstance()
+    {
+        $domain = new Domain('App\Test', '/path/to/test');
+
+        $this->assertEquals('App\Test', $domain->namespace);
+        $this->assertEquals('/path/to/test', $domain->realpath);
+        $this->assertEquals('/path/to/test/Database/Migrations', $domain->migration);
+    }
+
+    /**
+     * @return void
+     */
+    #[Test]
+    public function canGenerateCorrectPath()
+    {
+        $domain = new Domain('App\Test', '/base/path');
+
+        $result = $domain->path('controllers');
+
+        $this->assertEquals('/base/path/controllers', $result);
+    }
+
+    /**
+     * @return void
+     */
+    #[Test]
+    public function canConvertToArray()
+    {
+        $domain = new Domain('App\Test', '/test/path');
+        $domain->providers = ['TestProvider'];
+
+        $result = $domain->toArray();
+
+        $expectedArray = [
+            'realpath'  => '/test/path',
+            'namespace' => 'App\Test',
+            'providers' => ['TestProvider'],
+            'migration' => '/test/path/Database/Migrations',
+        ];
+
+        $this->assertEquals($expectedArray, $result);
+        $this->assertIsArray($result);
+        $this->assertArrayHasKey('realpath', $result);
+        $this->assertArrayHasKey('namespace', $result);
+        $this->assertArrayHasKey('providers', $result);
+        $this->assertArrayHasKey('migration', $result);
+    }
+
+    /**
+     * @return void
+     */
+    #[Test]
+    public function hasEmptyProvidersByDefault()
+    {
+        $domain = new Domain('App\Test', '/test');
+
+        $this->assertIsArray($domain->providers);
+        $this->assertEmpty($domain->providers);
+    }
+}
